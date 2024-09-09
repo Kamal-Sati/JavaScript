b89c55535b375b176db488142de3bf970013e5d4
Kamal-Sati <162813148+Kamal-Sati@users.noreply.github.com>
2024-09-09 23:11:49 +0530

    Add files via upload

---
+++ pr/App.js
@@ -0,0 +1,163 @@
+// src="https://unpkg.com/aos@next/dist/aos.js"
+// AOS.init({
+//     offset: 300,
+//     duration: 1000,
+//     once: true,
+
+// });
+
+
+
+
+// setTimeout(() => {
+//     document.querySelectorAll('[data-aos]').forEach(function (element) {
+//         if (!element.classList.contains('aos-animate')) {
+//             element.classList.add('aos-animate');
+//         }
+//     });
+// }, 1000);
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header__links", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".steps__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".service__container .service__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+
+
+ScrollReveal().reveal(".service__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".service__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".service__list li", {
+  ...scrollRevealOption,
+  delay: 1500,
+  interval: 500,
+});
+
+
+ScrollReveal().reveal(".experience__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+
+ScrollReveal().reveal(".dowonload__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".download__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".download__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".download__links", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+
+
+
+const menuBtn = document.querySelector('.open');
+const crossBtn = document.querySelector('.off');
+const main = document.querySelector('.nav_links');
+let isMenuOpen = false;
+
+// Open menu when menu button is clicked
+menuBtn.addEventListener('click', () => {
+  isMenuOpen = true;
+
+  // Show the cross button and hide the menu button
+  crossBtn.style.display = 'block';
+  menuBtn.style.display = 'none';
+
+  // Toggle the menu visibility
+  main.classList.add("ulActive");
+});
+
+// Close menu when cross button is clicked
+crossBtn.addEventListener('click', () => {
+  isMenuOpen = false;
+
+  // Hide the cross button and show the menu button
+  crossBtn.style.display = 'none';
+  menuBtn.style.display = 'block';
+
+  // Hide the menu
+  main.classList.remove("ulActive");
+});
+
+// Close the menu when any link is clicked
+main.addEventListener('click', () => {
+  if (isMenuOpen) {
+    // Close the menu
+    isMenuOpen = false;
+
+    // Hide the cross button and show the menu button
+    crossBtn.style.display = 'none';
+    menuBtn.style.display = 'block';
+
+    // Hide the menu
+    main.classList.remove("ulActive");
+  }
+});
+
+
+
+
+
+
+
+let clickBtn = document.querySelector(".btnfirst").addEventListener("click", function () {
+  let popup = document.querySelector(".Login_form")
+  popup.style.display = "block";
+  popup.style.opacity = 1;
+
+})
+
+let closeBtn = document.querySelector(".closeBtn").addEventListener("click", function () {
+  let popup = document.querySelector(".Login_form")
+  popup.style.display = "none";
+  popup.style.opacity = 0;
+
+
+})
