37b057f3a563464f7870e706a976339939650030
Kamal-Sati <162813148+Kamal-Sati@users.noreply.github.com>
2024-09-09 23:31:42 +0530

    Add files via upload

---
+++ pr7/script.js
@@ -0,0 +1,19 @@
+const div1 = document.querySelector(".div1").addEventListener("click" ,function (){
+        // div1.style.backgroundColor = "green";
+        document.body.style.backgroundColor = "green"
+})
+const div2 = document.querySelector(".div2").addEventListener("click" ,function (){
+    // div1.style.backgroundColor = "green";
+    document.body.style.backgroundColor = "grey"
+})
+const div3 = document.querySelector(".div3").addEventListener("click" ,function (){
+    // div1.style.backgroundColor = "green";
+    document.body.style.backgroundColor = "yellowgreen"
+})
+const div4 = document.querySelector(".div4").addEventListener("click" ,function (){
+    // div1.style.backgroundColor = "green";
+    document.body.style.backgroundColor = "red"
+})
+
+
+
