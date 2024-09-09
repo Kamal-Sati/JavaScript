e1067bd90c7469192f93016ea252c77a6f69b7c4
Kamal-Sati <162813148+Kamal-Sati@users.noreply.github.com>
2024-09-09 23:31:02 +0530

    Add files via upload

---
+++ pr6/script.js
@@ -0,0 +1,16 @@
+
+// const form = document.querySelector('#frommain')
+// const btn = document.querySelector('#submit');
+
+
+// btn.addEventListener('click', (e) => {
+//     // prevent the form from submitting
+//     // e.preventDefault();
+
+//     // show the form values
+//     const formData = new FormData(form);
+//     const values = [...formData.entries()];
+//     console.log(values);
+//     console.log("kamal");
+// });
+
